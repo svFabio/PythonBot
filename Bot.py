d74b9c7f19b2f53485c781bc4f12c4f9ea9076f4
svfabio <svfabio.andres@gmail.com>
2024-09-21 23:36:09 -0400

    Implementar sendMessage para enviar mensajes

--- Bot.py
+++ Bot.py
@@ -2,3 +2,12 @@ import requests
 import time
 
 
+def sendMessage(para, mensaje):
+    url = 'http: //localhost:3001/lead'
+    data = {
+        "message": mensaje,
+        "phone": para
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
